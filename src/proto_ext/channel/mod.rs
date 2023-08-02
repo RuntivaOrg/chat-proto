6dee01d6411cd2a9c97e6480e003af699cc2ff5d
runtiva <roger.canann@runtiva.com>
2023-08-02 11:36:17 -0500

    added proto_ext for new channel messages

--- src/proto_ext/channel/mod.rs
+++ src/proto_ext/channel/mod.rs
@@ -1,2 +1,8 @@
 pub mod create_ext;
 pub mod delete_ext;
+pub mod editabout_ext;
+pub mod editadmin_ext;
+pub mod editowner_ext;
+pub mod editparticipant_ext;
+pub mod editphoto_ext;
+pub mod edittitle_ext;
