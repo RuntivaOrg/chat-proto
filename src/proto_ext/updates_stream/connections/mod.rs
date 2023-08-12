b051eba7245e362cf4ef9e0e699ff92efe37cf8f
runtiva <roger.canann@runtiva.com>
2023-08-12 15:55:38 -0500

    added ext

--- src/proto_ext/updates_stream/connections/mod.rs
+++ src/proto_ext/updates_stream/connections/mod.rs
@@ -3,3 +3,4 @@ pub mod register_on_peer_list_ext;
 pub mod register_updates_connection_ext;
 pub mod unregister_from_peer_ext;
 pub mod unregister_from_peer_list_ext;
+pub mod unregister_updates_connection_ext;
---
+++ src/proto_ext/updates_stream/connections/unregister_updates_connection_ext.rs
@@ -0,0 +1,41 @@
+use crate::updates_stream as proto;
+
+use crate::proto_ext::updates_stream::{DataGetter, HeaderGetter, NatsRequestSetter};
+
+// ***********************************  Request Getters ***********************************
+// EditAbout Request Data message
+impl DataGetter<proto::UpdatesConnection> for proto::NatsUnregisterUpdatesConnectionRequest {
+    fn to_data(self) -> Option<proto::UpdatesConnection> {
+        self.data
+    }
+}
+
+// EditAbout Request Headers
+impl HeaderGetter for proto::NatsUnregisterUpdatesConnectionRequest {
+    fn headers(&self) -> &Vec<proto::MetadataMap> {
+        &self.headers
+    }
+}
+
+// ********************************** NATS Request Setter **********************************
+impl NatsRequestSetter<proto::UpdatesConnection, proto::NatsUnregisterUpdatesConnectionRequest>
+    for proto::NatsUnregisterUpdatesConnectionRequest
+{
+    fn from_headers_and_message(
+        headers: impl Into<Vec<proto::MetadataMap>>,
+        data: impl Into<proto::UpdatesConnection>,
+    ) -> Self {
+        proto::NatsUnregisterUpdatesConnectionRequest {
+            headers: headers.into(),
+            data: Some(data.into()),
+        }
+    }
+}
+
+// ***********************************  Response Getters ***********************************
+//
+// *** Uses the generic implementations from chat-proto\src\proto_ext\empty_response_ext.rs
+
+// ***********************************  Response Setters ***********************************
+//
+// *** Uses the generic implementations from chat-proto\src\proto_ext\error_response_ext.rs
