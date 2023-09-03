fab1cb8883dd54d7990f9dce8d2b3343104564dc
runtiva <roger.canann@runtiva.com>
2023-09-03 18:43:28 -0500

    Added User_GetState rpc

---
+++ src/proto_ext/chat/user/get_user_state_ext.rs
@@ -0,0 +1,87 @@
+use crate::runtiva::{nats::v1 as proto_nats, user::v1 as proto_user};
+
+use crate::proto_ext::{
+    DataGetter, DataSetter, ErrorGetter, ErrorSetter, HeaderGetter, NatsRequestSetter,
+};
+
+// ***********************************  Request Getters ***********************************
+// Create Request Data message
+impl DataGetter<proto_user::GetUserStateRequest> for proto_nats::NatsGetUserStateRequest {
+    fn to_data(self) -> Option<proto_user::GetUserStateRequest> {
+        self.data
+    }
+}
+
+// Create Request Headers
+impl HeaderGetter for proto_nats::NatsGetUserStateRequest {
+    fn headers(&self) -> &[proto_nats::MetadataMap] {
+        &self.headers
+    }
+
+    fn take_headers(&mut self) -> Vec<proto_nats::MetadataMap> {
+        let mut swapped = vec![];
+        std::mem::swap(&mut self.headers, &mut swapped);
+        swapped
+    }
+}
+
+// ********************************** NATS Request Setter **********************************
+impl NatsRequestSetter<proto_user::GetUserStateRequest, proto_nats::NatsGetUserStateRequest>
+    for proto_nats::NatsGetUserStateRequest
+{
+    fn from_headers_and_message(
+        headers: impl Into<Vec<proto_nats::MetadataMap>>,
+        data: impl Into<proto_user::GetUserStateRequest>,
+    ) -> Self {
+        proto_nats::NatsGetUserStateRequest {
+            headers: headers.into(),
+            data: Some(data.into()),
+        }
+    }
+}
+
+// ***********************************  Response Getters ***********************************
+// Create Response Data setter
+impl DataGetter<proto_user::GetUserStateResponse> for proto_nats::NatsGetUserStateResponse {
+    fn to_data(self) -> Option<proto_user::GetUserStateResponse> {
+        match self.msg {
+            Some(proto_nats::nats_get_user_state_response::Msg::Data(data)) => Some(data),
+            _ => None,
+        }
+    }
+}
+
+// Create Response Error setter
+impl ErrorGetter for proto_nats::NatsGetUserStateResponse {
+    fn error(&self) -> Option<&proto_nats::ErrorReply> {
+        match &self.msg {
+            Some(proto_nats::nats_get_user_state_response::Msg::Error(e)) => Some(e),
+            _ => None,
+        }
+    }
+}
+
+// ***********************************  Response Setters ***********************************
+// Create Response Data setter
+impl DataSetter<proto_user::GetUserStateResponse, proto_nats::NatsGetUserStateResponse>
+    for proto_nats::NatsGetUserStateResponse
+{
+    fn set_data(data: impl Into<proto_user::GetUserStateResponse>) -> Self {
+        let data = data.into();
+        proto_nats::NatsGetUserStateResponse {
+            msg: Some(proto_nats::nats_get_user_state_response::Msg::Data(data)),
+        }
+    }
+}
+
+// Create Response Error setter
+impl ErrorSetter<proto_nats::ErrorReply, proto_nats::NatsGetUserStateResponse>
+    for proto_nats::NatsGetUserStateResponse
+{
+    fn set_error(error: impl Into<proto_nats::ErrorReply>) -> Self {
+        let error = error.into();
+        proto_nats::NatsGetUserStateResponse {
+            msg: Some(proto_nats::nats_get_user_state_response::Msg::Error(error)),
+        }
+    }
+}
--- src/proto_ext/chat/user/mod.rs
+++ src/proto_ext/chat/user/mod.rs
@@ -1,5 +1,6 @@
 pub mod create_ext;
 pub mod delete_ext;
 pub mod get_user_chats_ext;
+pub mod get_user_state_ext;
 pub mod get_userprofile_min_ext;
 pub mod heartbeat_ext;
